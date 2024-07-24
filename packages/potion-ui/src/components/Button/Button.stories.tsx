022476365d02e3c2403e77b5beb18bb5f52d9600
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-24 20:50:00 +0200

    feat(ui): adding the basics for Button component

--- packages/potion-ui/src/components/Button/Button.stories.tsx
+++ packages/potion-ui/src/components/Button/Button.stories.tsx
@@ -1,20 +1,70 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from '.';
+import type { Meta, StoryObj } from "@storybook/react";
+import { FaCoffee, FaHeart, FaBasketballBall } from "react-icons/fa";
+import { Button } from ".";
 
 const meta: Meta<typeof Button> = {
-  title: 'Components/Button',
+  title: "Components/Button",
   component: Button,
   argTypes: {
-    label: { control: 'text' },
-    onClick: { action: 'clicked' },
+    label: { control: "text" },
+    size: { control: "select", options: ["default", "small", "large"] },
+    onClick: { action: "clicked" },
+    iconLeft: { 
+      options: ['None', 'Heart', 'Coffee', 'Ball'],
+      mapping: {
+        None: undefined,
+        Heart: FaHeart,
+        Coffee: FaCoffee,
+        Ball: FaBasketballBall,
+      },
+      control: { 
+        type: 'select' 
+      }
+    },
+    iconRight: { 
+      options: ['None', 'Heart', 'Coffee', 'Ball'],
+      mapping: {
+        None: undefined,
+        Heart: FaHeart,
+        Coffee: FaCoffee,
+        Ball: FaBasketballBall,
+      },
+      control: { 
+        type: 'select' 
+      }
+    },
   },
 };
 
 export default meta;
+
 type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
   args: {
-    label: 'Button',
+    label: "Hello world",
+    size: "default",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    ...Primary.args,
+    size: "small",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    ...Primary.args,
+    size: "large",
+  },
+};
+
+export const WithIcons: Story = {
+  args: {
+    ...Primary.args,
+    iconLeft: FaCoffee,
+    iconRight: FaHeart,
   },
 };
--- packages/potion-ui/src/components/Button/index.tsx
+++ packages/potion-ui/src/components/Button/index.tsx
@@ -1,17 +1,82 @@
-import React from 'react';
+import React from "react";
+import { Typography } from "../Typography";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../../lib";
 
-interface ButtonProps {
+const buttonVariants = cva(
+  "flex flex-row space-x-2 items-center justify-center rounded-[3px] transition-all duration-300",
+  {
+    variants: {
+      variant: {
+        default:
+          "text-white bg-product-300 hover:bg-product-400 active:bg-product-500",
+      },
+      size: {
+        default: "px-4 py-3",
+        small: "px-3 py-2",
+        large: "px-6 py-4",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+const iconVariants = cva("", {
+  variants: {
+    size: {
+      small: "w-3 h-3",
+      default: "w-4 h-4",
+      large: "w-5 h-5",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+const labelVariants = cva("font-normal", {
+  variants: {
+    size: {
+      small: "text-sm",
+      default: "text-base",
+      large: "text-lg",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   label: string;
-  onClick?: () => void;
+  iconLeft?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  iconRight?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  variant,
+  size,
+  label,
+  iconLeft: IconLeft,
+  iconRight: IconRight,
+  onClick,
+  ...props
+}) => {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      {...props}
       onClick={onClick}
+      className={cn(buttonVariants({ variant, size, className }))}
     >
-      {label}
+      {IconLeft && <IconLeft className={cn(iconVariants({ size }))} />}
+      <Typography className={cn(labelVariants({ size }))}>{label}</Typography>
+      {IconRight && <IconRight className={cn(iconVariants({ size }))} />}
     </button>
   );
 };
