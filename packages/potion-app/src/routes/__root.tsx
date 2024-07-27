66412693cfb22391f91ce62c0742a009024d079d
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-27 12:44:43 +0200

    feat(app): setting up tanstack router

---
+++ packages/potion-app/src/routes/__root.tsx
@@ -0,0 +1,27 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/ingredients" className="[&.active]:font-bold">
+          About
+        </Link>
+        <Link
+          params={{ potionId: "123" }}
+          to="/potion/$potionId"
+          className="[&.active]:font-bold"
+        >
+          Potion Details
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
