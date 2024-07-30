2a507545200cbecf6a180f4d24ce0eee53910118
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-30 08:25:10 +0200

    chore(api): prepare for deployment to vercel

--- packages/potion-api/src/index.ts
+++ packages/potion-api/src/index.ts
@@ -2,16 +2,30 @@ import "dotenv/config";
 import { serve } from "@hono/node-server";
 import routes from "./routes";
 import prisma from "./lib/prisma";
+import { Hono } from "hono";
 
-const port = Number(process.env.PORT ?? 80) || undefined;
-console.log(`Server is running on port ${port}`);
+const app = new Hono();
 
-serve({
-  fetch: routes.fetch,
-  port,
+app.route("/", routes);
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  return c.text("Internal Server Error", 500);
 });
 
-process.on("SIGINT", async () => {
-  await prisma.$disconnect();
-  process.exit(0);
-});
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
+  const port = Number(process.env.PORT ?? 80) || undefined;
+  console.log(`Server is running on port ${port}`);
+
+  serve({
+    fetch: app.fetch,
+    port,
+  });
+
+  process.on("SIGINT", async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
+}
