de08908f85f157221d0b23ba178dcedc10cb0671
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-30 11:50:45 +0200

    chore(app): modify build process

--- packages/potion-app/vite.config.ts
+++ packages/potion-app/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "potion-ui": path.resolve(__dirname, "../potion-ui"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["potion-ui"],
+    },
+  },
 });
