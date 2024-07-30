84b56f9d02f1975569a808fa2bcc36298e23f0b9
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-30 08:44:38 +0200

    chore(api): adjust deployment for vercel

--- packages/potion-api/src/index.ts
+++ packages/potion-api/src/index.ts
@@ -1,19 +1,20 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
+import { handle } from "@hono/node-server/vercel";
 import routes from "./routes";
 import prisma from "./lib/prisma";
 import { Hono } from "hono";
 
 const app = new Hono();
 
-app.route("/", routes);
+app.route("", routes);
 
 app.onError((err, c) => {
   console.error(`${err}`);
   return c.text("Internal Server Error", 500);
 });
 
-export default app;
+export default handle(app);
 
 if (process.env.NODE_ENV !== "production") {
   const port = Number(process.env.PORT ?? 80) || undefined;
