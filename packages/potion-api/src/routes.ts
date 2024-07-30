1c376efcea279f5ab09e8a96e2ce12c2b5b5fa83
Mohammed Jemmoudi <jemmoudi.med@proton.me>
2024-07-30 09:11:05 +0200

    chore(vercel): add file extention while importing

--- packages/potion-api/src/routes.ts
+++ packages/potion-api/src/routes.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { z } from "zod";
-import prisma from "./lib/prisma";
+import prisma from "./lib/prisma.js";
 
 const app = new Hono().basePath("/api");
 
